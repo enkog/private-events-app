38a66910821659d5a761f776cacdf1f585a25159
enkog <nkirukaogu@gmail.com>
2021-02-18 20:53:02 +0100

    Add the show and create actions, flash and error messages

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,24 @@
+class EventsController < ApplicationController
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+        flash[:notice] = "Event created"
+        redirect_to event_path(@event)
+    else
+        render 'new'
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+    def event_params
+      params.require(:event).permit(:title, :description, :location, :date)
+    end
+end
