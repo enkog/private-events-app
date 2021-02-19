4683732ee79cc52bff3ff44766900ea654ac9437
enkog <nkirukaogu@gmail.com>
2021-02-19 15:44:57 +0100

    Style form for creating events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
