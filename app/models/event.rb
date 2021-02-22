77c268889d55fdce2ef6134228b9415193645139
enkog <nkirukaogu@gmail.com>
2021-02-22 15:02:24 +0100

    Add event_users association

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :event_users
+  has_many :attendees, through: :event_users, source: :user
+
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
   validates :user_id, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  has_many :events
+  has_many :events, class_name: 'Event', foreign_key: 'creator_id'
+  has_many :event_users
+  has_many :created_events, through: :event_users, source: :event
+
   validates :name, presence: true, uniqueness: true, length: {minimum: 3, maximum: 20}
   
   has_secure_password
