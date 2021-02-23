a6c3cc9256c07e6cde599257ad92855a4a0534fa
enkog <nkirukaogu@gmail.com>
2021-02-23 03:38:45 +0100

    Add scope to events for past and upcoming events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :events, class_name: 'Event', foreign_key: 'creator_id'
+  has_many :created_events, class_name: 'Event', foreign_key: :creator_id
   has_many :event_users
-  has_many :created_events, through: :event_users, source: :event
+  has_many :attended_events, through: :event_users, source: :event
 
   validates :name, presence: true, uniqueness: true, length: {minimum: 3, maximum: 20}
   
