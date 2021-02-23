d0f318c65d7372e694c7bba45896e4d7a372f553
enkog <nkirukaogu@gmail.com>
2021-02-23 04:50:55 +0100

    Add tests to user model

---
+++ test/models/user_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+  def setup
+    @user = User.new(name: "nkiruka")
+  end
+
+  test 'user should be valid' do
+    assert true
+  end
+
+  test "username should be present" do
+    @user.name = " "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 21
+    assert_not @user.valid?
+  end
+
+  test "name should not be too short" do
+    @user.name = "aa"
+    assert_not @user.valid?
+  end
+
+end
